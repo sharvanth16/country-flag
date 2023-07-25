2b07a423cfb5b29779935086f60500fac4921cab
sharvanth16 <138028775+sharvanth16@users.noreply.github.com>
2023-07-25 11:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 var res = fetch("https://restcountries.com/v2/all");
-res.then((data)=>data.json()).then((data1)=>{
-    for(var i=0; i<data1.length; i++){
-        console.log(data1[i].name);
-        var div = document.createElement("div");
-        div.innerHTML= `<div class="card" style="width: 18rem;">
-        <img src= 
+res.then((data) => data.json()).then((data1) => {
+  for (var i = 0; i < data1.length; i++) {
+    console.log(data1[i].name);
+    var div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <img src="${data1[i].flag}" class="card-img-top" alt="Flag of ${data1[i].name}">
         <div class="card-body">
-          <img src=${data1[i].flag} class="card-img-top" alt="...">
           <h5 class="card-title">Country Name</h5>
           <h6 class="card-subtitle mb-2 text-muted">${data1[i].name}</h6>
-          <p class="card-text">${data1[i].region}
-          ${data1[i].subregion}
+          <p class="card-text">
+            <span style="color: black">${data1[i].region}</span><br>
+            <span style="color: black">${data1[i].subregion}</span>
           </p>
           <a href="#" class="card-link">Card link</a>
           <a href="#" class="card-link">Another link</a>
         </div>
-      </div> `
+      </div> 
+    `;
 
-        document.body.append(div);
-        div.style.color
-    }
+    document.body.append(div);
+  }
 });
+
